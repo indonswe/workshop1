c818de2644f02f96844702ed74f285423d5625da
indonswe <afc2002afc@hotmail.com>
2021-11-09 16:57:20 +0100

    first commit

---
+++ src/components/NameList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+/*const names = ["Mehrdad Javan", "Ulf Bengtsson", "Simon Elbrink"];
+
+const getLastName = (name) => {
+    let lastName = name.split(" ")[1];
+    return <span>{lastName}</span>
+};*/
+
+const NameList = () => {
+
+    return (
+    <h1>Hello</h1>
+    );
+};
+
+export default NameList;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+//import ArrowFunctionApp from './ex3-arrow-function/ArrowFunctionApp';
+//import Table from './ex4-table/Table';
+//import "bootstrap/dist/css/bootstrap.css";
+//import MyApp from './ex4-table/MyApp';
+//import NameList from './ex5-functions-variables/NameList';
+
+ReactDOM.render(<NameList />, document.getElementById("root"));
