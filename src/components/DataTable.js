3da45d7818b23e9a232199e9f0852327ff6399b8
indonswe <afc2002afc@hotmail.com>
2021-11-10 09:31:59 +0100

    is not defined

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -2,30 +2,61 @@ import React from 'react';
 //import TableRow from './TableRow';
 
 
+const TableAction = () => {
+    
+    return (<button type="button" className="btn btn-danger">Delete</button>);
+};
 
-    /*const studentList = [
-        {
-            id: 1,
-            firstName: Karl,
-        },
-        {
-            id: 2,
-            firstName: Dorian,
-        },
-        {
-            id: 3,
-            firstName: Kalle,
-        },
-    ]
-    const [students, setStudents] = useState(studentList);
+const TableHeader = () => {
+    return (
+    <thead>
+        <tr>
+            <th style={ {color: 'red'} }>Id</th>
+            <th className={'test'}>Name</th>
+            <th>TableAction</th>
+        </tr>
+    </thead>
+    );
+}
 
-    const listOfStudents = <TableRow students = {studentList}/>;*/
+const TableRow = (props) => {
+    return (
+        <ul>
+            {
+                props.students.map( (student) => (<li>{student}</li>))
+                //props.students.map( (student) => (<li>{getLastName(student)}</li>))
+            }
+        </ul>
+    );
+};
 
 
-    //const [studentList, setStudentList] = useState(initialData);
+
+//const [studentList, setStudentList] = useState(initialData);
     
     
     const DataTable = () => {
+
+
+        const studentList = [
+            {
+                id: 1,
+                firstName: Karl,
+            },
+            {
+                id: 2,
+                firstName: Dorian,
+            },
+            {
+                id: 3,
+                firstName: Kalle,
+            },
+        ]
+        const [students, setStudents] = useState(studentList);
+    
+        const listOfStudents = <TableRow students = {studentList}/>;
+
+
     return (
 
         <div>
